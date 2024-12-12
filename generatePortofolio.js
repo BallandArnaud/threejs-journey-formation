9bfc3920146e4494302f0a9a19981ce83c5361ae
Arnaud Balland <balland.arnaud@gmail.com>
2024-12-12 14:29:02 +0100

    add : deploy

---
+++ generatePortofolio.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+const demoDir = path.join(__dirname, "demo");
+const assetsDir = path.join(__dirname, "assets");
+
+const projects = fs
+    .readdirSync(demoDir)
+    .filter((file) => fs.statSync(path.join(demoDir, file)).isDirectory())
+    .map((project) => ({
+        name: project,
+        image: `assets/${project}.png`,
+    }));
+
+fs.writeFileSync(
+    path.join(__dirname, "projects.json"),
+    JSON.stringify(projects, null, 2)
+);
+
+console.log("projects.json generated!");
